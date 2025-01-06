b68e3a351e4c2614320582b317488e6f9e95663c
The_Fiafi <thewaffelfiafi@gmail.com>
2025-01-06 20:24:28 +0300

    add parallax

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import SpellWardLogo from "@/static/spellward_logo.png";
+
 import "./App.scss";
+
+import TitleParallax from "./components/PageParts/TitleParallax/TitleParallax";
 
 
 export default function App() {
     return (
-        <div>
-
+        <div className="app">
+            <div className="title-container">
+                <div className="spellward-title-logo-container">
+                    <img src={SpellWardLogo} className="spellward-title-logo"/>
+                </div>
+        
+                <TitleParallax offset={0} />
+            </div>
         </div>
     );
 };
